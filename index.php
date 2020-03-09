cc11825dfa05d29e49a3113126f1a4468dfcb484
Alex Fritz <fritz.alex67@gmail.com>
2020-03-09 11:05:55 +0100

    initialisation du projet avec index.php et styles.css

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <title>blog</title>
+    </head>
+    <body>
+        <header id="header">
+            <div id="banniere" >
+                <div id="presentation">
+                    <p>Jean Forteroche</p>
+                    <p>Auteur et écrivain</p>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Accueil</a></li>
+                        <li><a href="#">Qui suis-je ?</a></li>
+                        <li><a href="tousLesChapitres.php">Chapitres</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div id="titreRoman">
+                <h1>Billet simple pour l'Alaska</h1>
+                <p>Ecrit par Jean Forteroche</p>
+            </div>  
+        </header>
+        <section>
+            <div id="desc">
+                <div id="billets"></div>
+                <div id="billets"></div>
+                <div id="billets"></div>
+
+                <p><a href="tousLesChapitres.php">Tous les chapitres</a></p>
+            </div>
+            
+        </section>
+        <footer>
+            <div id="mentions">
+                
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li>/</li>
+                    <li><a href="#">Qui suis-je ?</a></li>
+                    <li>/</li>
+                    <li><a href="#">Blog</a></li>
+                    <li>/</li>
+                    <li><a href="#">Contact</a></li>
+                    <li>/</li>
+                    <li><a href="#">Mentions Légales</a></li>
+                </ul>
+                
+                <div id="reseaux">
+                    <p>Suivez moi sur:</p>
+                    <div >
+                        <img src="image/facebook-icon.png">
+                        <img src="image/instagram-icon.png">
+                        <img src="image/Twitter-icon.png">
+                    </div>
+                </div>
+            </div>
+            <div id="copywright">
+                <p>©2020 - Tout droits réservés</p>
+                <p>Site réalisé par Alex Fritz dans le cadre d'une formation</p>
+            </div>
+        </footer>
+        
+    </body>
+</html>
