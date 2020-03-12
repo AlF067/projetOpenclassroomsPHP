c28c513d16bafb2f35658fe5bf514e8aa9085941
Alex Fritz <fritz.alex67@gmail.com>
2020-03-12 16:26:30 +0100

    initialisation de la mise en place des commentaires aux chapitres

--- vue/vueChapitresChoisis.php
+++ vue/vueChapitresChoisis.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" type="text/css" href="../styles/stylesGeneral.css">
-        <link rel="stylesheet" type="text/css" href="../styles/stylesChapitresChoisis.css">
-        <title>blog</title>
-    </head>
-    <body>
-        <?php require "header.php" ?>
-        <section>
+
+
+<?php $linkStylesGeneral = '<link rel="stylesheet" type="text/css" href="../styles/stylesGeneral.css">' ?>
+<?php $linkStyles = '<link rel="stylesheet" type="text/css" href="../styles/stylesChapitresChoisis.css">' ?>
+<?php ob_start();  ?>            
             <div id="lecture">
                <?php 
-                        
-
                         $donnees = $chapitresChoisis->fetch();
-                        
+                        $title = $donnees['titre'] ;
+
                         ?>
                         <h2><?php echo $donnees['titre']. " " . " ajouté le " . $donnees['dateAjout']?></h2>
                         <p><?php echo $donnees['histoire'] ?></p>
@@ -25,9 +18,36 @@
                     ?>
 
                     <div><a href="../controleur/chapitres.php">Retour au choix des chapitres</a></div>   
-            </div>     
-        </section>
-        <?php require "footer.php" ?>
-        
-    </body>
-</html>
+            </div>
+            <div id="rubriqueCommentaires">
+                <div>
+                    <form method="POST" action="">
+                        <h3>Laisser un commentaire</h3>
+                        <input type="text" name="pseudo" placeholder="Votre pseudo">
+                        <textarea placeholder="Votre commentaire"></textarea>
+                        <input type="submit" name="envoi" id="envoi">
+                    </form>                    
+                </div>
+                <div id="commentaires">
+                    <h3>Les Commentaires</h3>
+                    <div>
+                        
+                        <h4>AlF : <span>le 03/10/12</span></h4>
+                        <p>ca c'est mon commentaire !</p>
+                    </div>  
+                    <div>
+                        
+                        <h4>AlF : <span>le 03/10/12</span></h4>
+                        <p>ca c'est mon commentaire !</p>
+                    </div>  
+                    <div>
+                        
+                        <h4>AlF : <span>le 03/10/12</span></h4>
+                        <p>ca c'est mon commentaire ca c'est mon commentaireca c'est mon commentaire !</p>
+                    </div>
+                                       
+                </div>
+                
+            </div> 
+<?php $contenu = ob_get_clean();  ?>
+<?php require 'gabarit.php'; ?>
