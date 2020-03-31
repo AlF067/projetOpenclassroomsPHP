4bae11313941c0a99cba59605d27ac3889f74ebf
Alex Fritz <fritz.alex67@gmail.com>
2020-03-31 16:31:17 +0200

    commencement de securisation coté utilisateur (commentaires et chapitre choisis)

--- controleur/chapitresChoisis.php
+++ controleur/chapitresChoisis.php
@@ -3,23 +3,41 @@ require "../modele/Chapters.php";
 require "../modele/Commentaires.php";
 require "../modele/Manager.php";
 $manager = new Manager;
-$chapitreChoisis = $manager->chaptre($_GET["idChapitre"]);
 
-/* Enregistre le signalement dans la BDD s'il y en a  */
-
-if (isset($_POST["signalement"])) {  
-  $manager->signaler($_POST["signalement"]);
+if (isset($_GET["idChapitre"])) {
+  $idChapitre = (int) $_GET["idChapitre"];
 }
-/* Pour la pagination des commentaires */
-if (isset($_POST['limitMin'])) {
- $limitMin = $_POST['limitMin'];
-}else {
-  $limitMin = 0;
-}
-/* Ajoute un commentaire s'il y en a un a ajouter */
-if (isset($_POST["idChapitre"]) && $_POST["pseudo"] && $_POST["commentaire"] ) {
-  $manager->ajoutCommentaires(($_POST["idChapitre"]), ($_POST["pseudo"]), ($_POST["commentaire"]));
+if (isset($_POST["idChapitre"])) {
+  $idChapitre = (int) $_POST["idChapitre"];
 }
 
+if (!($idChapitre > $manager->maxChaptres())) {
+  if (!($idChapitre == 0)) {
 
-require "../vue/vueChapitresChoisis.php";
+    $chapitreChoisis = $manager->chaptre($idChapitre);
+
+    /* Enregistre le signalement dans la BDD s'il y en a  */
+
+    if (isset($_POST["signalement"])) {
+      $signalement = (int ) $_POST["signalement"];
+      $manager->signaler($signalement);
+    }
+    /* Pour la pagination des commentaires */
+    if (isset($_POST['limitMin'])) {
+      $limitMin =(int) $_POST['limitMin'];
+    } else {
+      $limitMin = 0;
+    }
+    /* Ajoute un commentaire s'il y en a un a ajouter */
+    if (isset($_POST["idChapitre"]) && $_POST["pseudo"] && $_POST["commentaire"]) {
+      $pseudo = htmlspecialchars($_POST["pseudo"]);
+      $commentaire = htmlspecialchars($_POST["commentaire"]);
+      $manager->ajoutCommentaires($idChapitre, $pseudo, $commentaire);
+    }
+    require "../vue/vueChapitresChoisis.php";
+  }else {
+    require "../vue/vuePageErreur.php";
+  }
+} else {
+  require "../vue/vuePageErreur.php";
+}
---
+++ vue/vuePageErreur.php
@@ -0,0 +1,29 @@
+<?php $title = "Chapitre"; ?>
+<?php $linkStylesGeneral = '<link rel="stylesheet" type="text/css" href="../styles/stylesGeneral.css">' ?>
+<?php $linkStyles = '<link rel="stylesheet" type="text/css" href="../styles/stylesChapitresChoisis.css">' ?>
+<?php ob_start();  ?>
+<div id="lecture">
+    <p>Ce chapitre n'existe pas malheureusement !</p>
+    <div><a href="../controleur/chapitres.php">Retour au choix des chapitres</a></div>
+</div>
+
+
+
+<div >
+    <div >
+        <div >
+            <h3>Les Commentaires</h3>
+            <div >
+                <p>Pas besoin de commentaires s'il n'y a pas de chapitre à lire !</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php $contenu = ob_get_clean();  ?>
+
+<?php require 'gabarit.php'; ?>
