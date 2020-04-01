a20ca2d6e6d4649f65ad44b2131f1bfb40b7851d
Alex Fritz <fritz.alex67@gmail.com>
2020-04-01 16:58:16 +0200

    modifications mineures

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@ require "../modele/Chapters.php";
 require "../modele/Manager.php";
 $manager = new Manager;
 
-
+ 
 /* Supprime un chapitre s'il y en a un a supprimer */
 if (isset($_POST["oui"])) {
     $manager->deleteChapitre($_POST["idChapitre"]);
@@ -17,7 +17,12 @@ if (isset($_POST["modifHistoire"]) && isset($_POST["modifTitre"])) {
 
 /* Ajoute un chapitre s'il y en a un a ajouter */
 if (isset($_POST["histoire"]) && isset($_POST["titre"])) {
-    $manager->addChaptre($_POST["idChapitre"], $_POST["titre"], $_POST["histoire"]);
+    if ($manager->chaptre($_POST["idChapitre"]) == false) {
+        $manager->addChaptre($_POST["idChapitre"], $_POST["titre"], $_POST["histoire"]);
+    }else {
+        $chapitreDejaExistant = "le numéro de chapitre existe déja";
+    }
+    
 }
 
 /* Variable necessaire pour afficher la page de la liste des chapitre (5 chapitres par page) */
--- admin/vue/vueAccueilAdmin.php
+++ admin/vue/vueAccueilAdmin.php
@@ -1,4 +1,4 @@
-<?php $titleAdmin = "Accueil Administrateur" ; ?>
+<?php $titleAdmin = "Accueil Administrateur"; ?>
 <?php $linkStylesGeneral = '<link rel="stylesheet" type="text/css" href="../styles/stylesAdmin.css">' ?>
 <?php $linkStyles = '<link rel="stylesheet" type="text/css" href="../styles/stylesChapitres.css">' ?>
 <?php ob_start();  ?>
@@ -7,52 +7,57 @@
     <p><a href="controleur/ajoutAdmin.php"><i class="far fa-plus-square"></i> Ajouter un article</a></p>
 </section>
 <section>
-    <?php    
+    <?php if (isset($chapitreDejaExistant)) {
+     ?>
+    <div id="chapitreDejaExistant">
+            <?php echo $chapitreDejaExistant ; ?>
+    </div>
+    <?php }
     foreach ($manager->listChaptres($limitMin, 5) as $obj) {
         $iconeSignalement = $manager->iconeSignalement($obj->idChapitre());
-        ?>
-        
-            <div class="blocChapitre">
-                <div class="chapitre">
-                    <h2><?php echo $obj->titre() . " " . " ajouté le " . $obj->dateAjout(). "<span id='iconeSignalement'> " . $iconeSignalement . "</span> "?></h2>
-                    <p><?php echo $obj->histoire() ?></p>
-                </div>
-                <div class="lireChapitre">
-                        <a href="controleur/moderationCommentairesAdmin.php?idChapitre=<?php echo $obj->idChapitre() ?>">Commentaires</a>
-                        <a href="vue/vueConfirmationSuppressionAdmin.php?idChapitre=<?php echo $obj->idChapitre() ?>">Supprimer</a>
-                        <a href="controleur/modificationChapitre.php?idChapitre=<?php echo $obj->idChapitre() ?>">Modifer</a>     
-                    </div>
-            </div>
-        <?php
-        
-        }                  
-   
     ?>
-<div id="paginationChapitres">
-    <?php
-    $pages = 0;
-    $limitMin = 0;
-    $commentairesParPage = 0;
-    echo "<div id='nombreDePages'>Page : </div>";
-    while ($limitMin < $manager->maxChaptres()) {
-    ?>
-        <?php
-        if (!$pages == 0) {
-            echo "<div class='slash'>/</div>";
-        } 
-        ?>
-        <div id="numerosPage">
-            <a href="../admin?limitMin=<?php echo $pages * 5 ?> "><?php echo $pages + 1; ?></a>
 
+        <div class="blocChapitre">
+            <div class="chapitre">
+                <h2><?php echo $obj->titre() . " " . " ajouté le " . $obj->dateAjout() . "<span id='iconeSignalement'> " . $iconeSignalement . "</span> " ?></h2>
+                <p><?php echo $obj->histoire() ?></p>
+            </div>
+            <div class="lireChapitre">
+                <a href="controleur/moderationCommentairesAdmin.php?idChapitre=<?php echo $obj->idChapitre() ?>">Commentaires</a>
+                <a href="vue/vueConfirmationSuppressionAdmin.php?idChapitre=<?php echo $obj->idChapitre() ?>">Supprimer</a>
+                <a href="controleur/modificationChapitre.php?idChapitre=<?php echo $obj->idChapitre() ?>">Modifer</a>
+            </div>
         </div>
-        
     <?php
-        $commentairesParPage += 5;
-        $limitMin += 5;
-        $pages++;
+
     }
+
     ?>
-</div>
+    <div id="paginationChapitres">
+        <?php
+        $pages = 0;
+        $limitMin = 0;
+        $commentairesParPage = 0;
+        echo "<div id='nombreDePages'>Page : </div>";
+        while ($limitMin < $manager->maxChaptres()) {
+        ?>
+            <?php
+            if (!$pages == 0) {
+                echo "<div class='slash'>/</div>";
+            }
+            ?>
+            <div id="numerosPage">
+                <a href="../admin?limitMin=<?php echo $pages * 5 ?> "><?php echo $pages + 1; ?></a>
+
+            </div>
+
+        <?php
+            $commentairesParPage += 5;
+            $limitMin += 5;
+            $pages++;
+        }
+        ?>
+    </div>
 </section>
 
 <?php $contenuAdmin = ob_get_clean();  ?>
